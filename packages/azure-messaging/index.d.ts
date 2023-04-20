42c96e8211bd4420ec6a4da7ea4de176e767fca6
Nathan Walker <walkerrunpdx@gmail.com>
2023-04-19 20:51:50 -0700

    chore: cleanup types

--- packages/azure-messaging/index.d.ts
+++ packages/azure-messaging/index.d.ts
@@ -1,8 +1,6 @@
 import { Observable } from '@nativescript/core';
 import { AzureMessagingCommon } from './common';
 
-export declare class AzureMessaging extends Observable implements AzureMessagingCommon {
+export declare class AzureMessaging extends AzureMessagingCommon {
   init(connectionString: string, hubName: string): Promise<void>;
-  register(deviceToken: string, tags: string[]): Promise<void>;
-  unregister(): Promise<void>;
 }
